6e52ea148f5b593f586e021e2a11ed174fa48ede
catalan <matheuscatalan@outlook.com>
2024-11-07 08:08:58 -0300

    chore: add tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <h1>Hello world</h1>
-  )
-}
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-500">Hello World</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
