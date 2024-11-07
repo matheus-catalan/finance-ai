0886f734da8386638a82564d3aca71ef5f6d0144
catalan <matheuscatalan@outlook.com>
2024-11-07 09:27:08 -0300

    feat: add authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-const Home = () => {
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
   return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">Hello World</h1>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
     </div>
   );
 };
